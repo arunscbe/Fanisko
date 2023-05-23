aeeba8cec468ab9dd7a94119f4a5a1462f17c4e1
Arunkumar <arunscbe@gmail.com>
2023-05-23 14:16:59 +0530

    player display

--- js/ui.js
+++ js/ui.js
@@ -1,17 +1,38 @@
 $(document).ready(function(){
-    var arr = ['1', '2', '3', '4', '5','6','ALL'];
+    var arr = ['1', '2', '3', '4','6','ALL'];
+    var test = [{
+        "number":1,
+        "color":"red",
+        "boundary":"green"
+    },{
+        "number":2,
+        "color":"yellow",
+        "boundary":"green"
+    },{
+        "number":3,
+        "color":"orange",
+        "boundary":"green"
+    }];
     scores(arr);
 })
 
 const scores = (data)=>{
     var cont = document.getElementById('footerContainer');
     var ul = document.createElement('ul');
-    ul.setAttribute('style', 'padding: 0; margin: 0;background-color:blue;width:100%;text-align:center;margin:auto');
+    ul.setAttribute('style', 'padding-left:15%;width:100%;text-align:center;margin:auto');
     ul.setAttribute('class', 'scoreList');
+    // test.map((data)=>{
+    //     var li = document.createElement('li');
+    //     li.innerHTML = data.number; 
+    //     li.setAttribute('style', 'display: inline-block;font-size: 40px;margin-left:15px; padding-left:40px; padding-right:40px;padding-top:10px;padding-bottom:10px; background-color:red; color:white; border-radius: 10px; font-family: Arial, sans-serif;');    
+    //     ul.appendChild(li);     
+    //     console.log(data.number);
+    // });
     for (i = 0; i <= data.length - 1; i++) {
+        
         var li = document.createElement('li');  
         li.innerHTML = data[i];    
-        li.setAttribute('style', 'display: inline-block;font-size: 40px;margin-left:15px; padding-left:40px; padding-right:40px;padding-top:10px;padding-bottom:10px; background-color:red; color:white; border-radius: 10px; font-family: Arial, sans-serif;');    
+        li.setAttribute('style', 'display: inline-block;font-size: 40px;margin-left:15px; padding-left:40px; padding-right:40px;padding-top:10px;padding-bottom:10px; background-color:black; color:white; border-radius: 10px; font-family: Arial, sans-serif;');    
         ul.appendChild(li);     
     }
     cont.appendChild(ul);      
