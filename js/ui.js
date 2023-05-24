140decf260df585fba0b79dea106b0876180353e
Arunkumar <arunscbe@gmail.com>
2023-05-24 10:21:14 +0530

    UI responsive

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
     var runData = [
-        {"run":1,"color":"white"},{"run":2,"color":"yellow"},{"run":3,"color":"yellow"},
-        {"run":4,"color":"blue"},{"run":6,"color":"red"},{"run":'ALL',"color":"grey"}
+        {"run":1,"color":"white","id":"one"},{"run":2,"color":"yellow","id":"two"},{"run":3,"color":"yellow","id":"three"},
+        {"run":4,"color":"blue","id":"four"},{"run":6,"color":"red","id":"six"},{"run":'ALL',"color":"grey","id":"all"}
     ];
     scores(runData);
+    $('.scoreList').click((e)=>{
+        e.preventDefault();
+        let _Data = e.target.id;
+        wagonWheelDisplay(_Data);
+    })
+   
 })
 
 const scores = (runData)=>{
@@ -15,10 +21,18 @@ const scores = (runData)=>{
         let li = document.createElement('li');
         li.innerHTML = data.run; 
         li.setAttribute('style', `display: inline-block;font-size: 40px;margin-left:15px; padding-left:40px; padding-right:40px;padding-top:10px;padding-bottom:10px; background-color:black; color:${data.color}; border-radius: 15px; border: 1px solid red;font-family: Arial, sans-serif;`);    
+        li.setAttribute('id', `${data.id}`);
         ul.appendChild(li);     
     });
     cont.appendChild(ul);      
 }
+const wagonWheelDisplay = (data) => {
+    console.log(data);
+}
 const playerDisplay = () => {
-    
+    console.log('ererer...');
+    // alert('eerer....');
+}
+const teamsScore =() => {
+   
 }
